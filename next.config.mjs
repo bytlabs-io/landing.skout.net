ad4d0e4d2f6acc3fce0d89b925e458cd02655fd7
logosrhema01 <achiampongk22@gmail.com>
2024-09-03 13:57:37 +0000

    output to docs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
 	reactStrictMode: true,
 	images: { unoptimized: true },
 	output: "export",
+	distDir: "docs",
 	webpack(config) {
 		config.module.rules.push({
 			test: /\.svg$/,
